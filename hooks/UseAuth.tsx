17afb1476c15e9cd69a8c05242eaa857fc65cb4e
Joel Hernandez <you@example.com>
2023-01-27 07:09:10 -0500

    add authentication to login form

--- hooks/UseAuth.tsx
+++ hooks/UseAuth.tsx
@@ -1,22 +1,33 @@
 import { useAuthStore } from "../store/store";
 import Swal from 'sweetalert2'
 import {kanbanApi} from '../api/kanbanApi';
+import { user } from "../types/types";
 
-interface user {
- name:string,
- email:string,
- password:string,
-}
 
 const UseAuth= () => {
 
   const {onChecking,onLogin,onLogOut,clearErrorMessage} = useAuthStore();
 
+  const startAuthentication = async({email,password}:user) =>{
+    onChecking();
+
+    try{
+      const {data} = await kanbanApi.post('/auth',{email,password});
+      localStorage.setItem('token',data.token);
+      onLogin({name:data.name,uid:data.uid});
+    }
+    catch(error:any){
+    console.log(error);
+    const {response} = error;
+    const errorMsg = response.data?.errors?.password?.msg || response.data.msg;
+    onHandleRequestError(errorMsg);
+    }
+  }
+
   const createUser = async ({name,email,password}:user) =>{
     onChecking();
     try{
       const {data} = await kanbanApi.post('/auth/register',{name,email,password});
-      console.log(data); 
       localStorage.setItem('token',data.token);
       onLogin({name:data.name,uid:data.uid});
     }
@@ -24,21 +35,43 @@ const UseAuth= () => {
       console.log(error);
       const {response} = error;
       const errorMsg = response.data.errors?.password?.msg || response.data.msg 
-     
-      Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text:errorMsg
-    })
-      onLogOut(errorMsg);
-
-       setTimeout(()=>{
-        clearErrorMessage();
-       },1000)
+      onHandleRequestError(errorMsg);
    }
   }
+  const checkAuthToken = async () =>{
+    const token = localStorage.getItem('token');
+    if(!token) return onLogOut('not token available');
+    
+    try{
+    const {data} = await kanbanApi.get('/auth/renew');
+    localStorage.setItem('token',data.token);
+    onLogin({name:data.name,uid:data.uid});
+    }
+    catch(error:any){
+    localStorage.clear();
+     onLogOut(error.msg);
+    }
+    
+    }
+
+  const onHandleRequestError = (errorMsg:string) =>{
+   
+    Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text:errorMsg
+  })
+  onLogOut(errorMsg);
+
+  setTimeout(()=>{
+   clearErrorMessage();
+  },1000)
+  }
+
     return {
+      startAuthentication,
       createUser,
+      checkAuthToken
       
     }
 }
