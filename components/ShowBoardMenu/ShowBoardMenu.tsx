905024803dc63e47a588e76d5f3fab6282b02faa
Joel Hernandez <you@example.com>
2023-01-31 07:30:18 -0500

    Add mobile header nav bar to dashboard page

---
+++ components/ShowBoardMenu/ShowBoardMenu.tsx
@@ -0,0 +1,11 @@
+import navigation from '../../styles/navigation.module.css';
+
+export const ShowBoardMenu = () => {
+  return (
+    <div className={navigation.show_board_menu}>
+        <span className={navigation.show_board_menu_button}></span>
+        <span className={navigation.show_board_menu_button}></span>
+        <span className={navigation.show_board_menu_button}></span>
+    </div>
+  )
+}
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,24 @@
-
+import Head from "next/head";
+import { MobileNavBar } from "../components/MobileNavBar";
 
 const dashboard = () => {
   return (
-    <h1>Dashboard Page</h1>
+    <>
+     <Head>
+     <title>Dashboard</title>
+      <meta name="author" content="Joel Hernandez"/>
+      <meta name="description" content='about kanban tasks'/>
+      <meta name="keywords" content="kanban,tasks,managment,manage tasks,organize tasks"/>
+    </Head>
+
+   <div className="wrapper">
+      <header>
+        <MobileNavBar/>
+      </header>
+     
+   </div>
+
+    </>
   )
 }
 
