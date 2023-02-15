628928955b98dc2a9a94be0232969877eeadf984
Joel Hernandez <you@example.com>
2023-02-15 07:01:48 -0500

    create create board button component

--- components/Board/Board.tsx
+++ components/Board/Board.tsx
@@ -1,6 +1,7 @@
 import { RenderBoardMenu } from './renderBoardMenu';
 import { DarkLightModeToogle } from './DarkLightModeToogle';
 import dashboard from '../../styles/dashboard.module.css';
+import { RenderCreateNewBoardButton } from '../RenderCreateNewBoardButton/RenderCreateNewBoardButton';
 
 export const Board = () => {
   
@@ -8,7 +9,7 @@ export const Board = () => {
     <div className={dashboard.board_container}>
       <section>
          <RenderBoardMenu/>
-        
+         <RenderCreateNewBoardButton/>
       </section>
 
       <section>
