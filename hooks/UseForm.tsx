a9b37e302c9e73008cb0fc728e50684fb9fa2182
Joel Hernandez <you@example.com>
2023-03-06 06:23:21 -0500

    add memo to create input list to reduce innecessary re-render

--- hooks/UseForm.tsx
+++ hooks/UseForm.tsx
@@ -1,15 +1,15 @@
 
-import { useState,ChangeEvent } from "react"
-
+import { useState,useCallback,ChangeEvent } from "react"
+import debounce from "just-debounce-it";
 export function UseForm<T> (initialState:T){
 
   const [formValues,setFormValues] = useState(initialState);
   const [formSubmitted,setFormSubmitted] = useState(false);
 
+
   const handleChange = ({target}:ChangeEvent<HTMLInputElement>):void => {
      const {name,value}  = target;
-     
-     setFormValues({...formValues,[name]:value});
+      setFormValues({...formValues,[name]:value});
   }
 
   return{
