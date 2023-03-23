368d4fd9d025b90440a9e0203948032ec323f7d8
Joel Hernandez <you@example.com>
2023-03-23 10:16:49 -0400

    add validation when update form validation to the update board form

--- components/UpdateBoard/UpdateBoard.tsx
+++ components/UpdateBoard/UpdateBoard.tsx
@@ -1,5 +1,4 @@
-
-import { useEffect,ChangeEvent, useState } from "react";
+import { useEffect,ChangeEvent, useState,FormEvent } from "react";
 import {useFetchBoard, useInputList} from "../../hooks";
 import { CreateInputList } from "../CreateInputsLists/CreateInputList";
 import layout from "../../styles/layouts.module.css";
@@ -8,9 +7,10 @@ import share from '../../styles/share.module.css';
 
 export const UpdateBoard = () => {
 
-   const {insertEntireInputList} = useInputList();
+   const {insertEntireInputList,areInputListItemsValid,updateIsCurrentInputEmpty} = useInputList();
    const {board_columns,board_name,isLoading,isSuccess} = useFetchBoard();
    const [boardNameInput,setBoardNameInput] = useState('');
+   const [isFormSubmitted,setIsFormSubmitted] = useState(false);
    
    useEffect(()=>{
     if(isSuccess){
@@ -25,20 +25,33 @@ export const UpdateBoard = () => {
     setBoardNameInput(target.value);
   }
 
+  const onSubmitUpdateBoardForm = (event:FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+
+    if(boardNameInput !=='' && areInputListItemsValid()){
+      console.log('form can be sent...');
+        setIsFormSubmitted(false);
+      return;
+    }
+    console.log('form cannot be submitted');
+    setIsFormSubmitted(true);
+    updateIsCurrentInputEmpty(true);
+
+  }
+
   return (
-   
-   <form className={layout.modal_form}>
+   <form className={layout.modal_form} onSubmit={onSubmitUpdateBoardForm}>
     <div className={`${share.d_flex} ${share.d_flex_col} ${share.form_field} `}>
     <label className={share.label}>
       Board Name</label>
       <input
-       className={`${share.primary_input} ${share.d_flex_grow}`}
+        className={`${share.primary_input} ${share.d_flex_grow} ${isFormSubmitted && boardNameInput==='' && share.invalid_input}`}
         type = "text"
         id="boardName"
         name="boardName"
         value={boardNameInput}
         onChange = {updateBoardNameInput}
-        placeholder = "e.g. Web Design"
+        placeholder = {boardNameInput ==='' ? "cannot be empty":"e.g. Web Design"}
      />
     </div>
     
