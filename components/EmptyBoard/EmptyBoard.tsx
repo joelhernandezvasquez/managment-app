635d33fc37b0b9132ecbcb65775e474bad676033
Joel Hernandez <you@example.com>
2023-04-18 06:29:28 -0400

    make the button add new task to open  a new task on the empty board

--- components/EmptyBoard/EmptyBoard.tsx
+++ components/EmptyBoard/EmptyBoard.tsx
@@ -1,17 +1,35 @@
+import { useModal } from '../../hooks';
+import { ModalFormLayout } from '../Layout/ModalFormLayout';
 import button from '../../styles/buttons/buttons.module.css';
 import style from '../../styles/dashboard.module.css';
+import { AddNewTask } from '../AddNewTask/AddNewTask';
 
 export const EmptyBoard = () => {
+
+  const {isModalOpen,toggleModal} = useModal();
+
   return (
     <div className={style.empty_board}>
-       <p>
-         This board is empty. Create a new column to get started.
+       <p className={style.empty_board_message}>
+         This board is empty. Create a new task to get started.
        </p>
 
-       <button className={`${button.btn_primary} ${button.add_column_btn}`}>
+       <button 
+        className={`${button.btn_primary} ${button.add_column_btn}`}
+        onClick = {toggleModal}
+        >
         <span>+</span>
-        Add new Column
+        Add task
        </button>
+     
+     {
+      isModalOpen && (
+        <ModalFormLayout formTitle='Add New Task'>
+          <AddNewTask closeWindow={toggleModal}/>
+        </ModalFormLayout>
+      )
+     }
+
     </div>
   )
 }
