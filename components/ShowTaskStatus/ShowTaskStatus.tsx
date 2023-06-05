fa73afa2c79a093c8916348358973cc30c15c41f
Joel Hernandez <you@example.com>
2023-06-05 07:25:08 -0400

    refactor show task status component

--- components/ShowTaskStatus/ShowTaskStatus.tsx
+++ components/ShowTaskStatus/ShowTaskStatus.tsx
@@ -1,17 +1,27 @@
-import {FC,useState,memo,useMemo} from 'react';
+import {FC,useState,memo,useMemo,useEffect} from 'react';
 import { useModal,useFetchBoard } from '../../hooks';
 import { Status } from '../../types/types';
 import dashboard from '../../styles/dashboard.module.css';
 import { mappedListOfStatus } from '../../helpers';
+import { v4 as uuidv4 } from 'uuid';
 interface StatusProps {
+ statusSent?:string,
  setTaskStatus:(status:string) => void
 }
 
-export const ShowTaskStatus:FC<StatusProps> = memo( ({setTaskStatus}) => {
-  
+export const ShowTaskStatus:FC<StatusProps> = memo( ({setTaskStatus,statusSent}) => {
     const {board_columns} = useFetchBoard();
-    const [currentStatus,setCurrentStatus] = useState({id:'0',status:'Select a Status'});
+    const [currentStatus,setCurrentStatus] = useState<Status>();
     const {isModalOpen,toggleModal} = useModal();
+
+    useEffect(()=>{
+      if(statusSent){
+        setCurrentStatus({id:uuidv4(),status:statusSent});
+        setTaskStatus(statusSent);
+        return;
+      }
+       setCurrentStatus({id:'0',status:'Select a Status'});
+    },[])
 
     const listOfStatus = useMemo(()=>{
       return mappedListOfStatus(board_columns ?? [])
@@ -25,6 +35,7 @@ export const ShowTaskStatus:FC<StatusProps> = memo( ({setTaskStatus}) => {
 
     return (
    <section>
+  
        <div className={dashboard.header_status_list} onClick={toggleModal}>
           <span className={dashboard.status_list_item}>{currentStatus?.status}</span>
           <svg  width="9" height="7" viewBox="0 0 9 7" fill="none">
